35c42f206b0df649959ab737950aa28157476089
Minh Pham <pdminh0410@gmail.com>
2024-01-05 20:39:41 -0800

    Section 6.65: Animated Widgets In Flutter

--- animdo/lib/pages/home_page.dart
+++ animdo/lib/pages/home_page.dart
@@ -1,17 +1,62 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
-    return _HomePageState();  
+    return _HomePageState();
   }
 }
 
 class _HomePageState extends State<HomePage> {
+  double _buttonRadius = 100;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            _pageBackground(),
+            _circularAnimatiionButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _pageBackground() {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+
+  Widget _circularAnimatiionButton() {
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            _buttonRadius += _buttonRadius == 200 ? -100 : 100;
+          });
+        },
+        child: AnimatedContainer(
+          duration: const Duration(seconds: 2),
+          curve: Curves.bounceInOut,
+          height: _buttonRadius,
+          width: _buttonRadius,
+          decoration: BoxDecoration(
+            color: Colors.purple,
+            borderRadius: BorderRadius.circular(100),
+          ),
+          child: const Center(
+              child: Text(
+            "Basic!",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          )),
+        ),
+      ),
+    );
   }
 }
