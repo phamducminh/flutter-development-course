0352abcbf20acec52d799f1aa4a8bc9a552d3b38
Minh Pham <pdminh0410@gmail.com>
2024-01-05 21:34:06 -0800

    Section 7.70: Creating Flutter App & Adding Dependencies

---
+++ coincap/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:coincap/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CoinCap',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        scaffoldBackgroundColor: const Color.fromRGBO(
+          88,
+          60,
+          197,
+          1.0,
+        ),
+      ),
+      home: HomePage(),
+    );
+  }
+}
