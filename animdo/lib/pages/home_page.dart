243c7cfa12f410e7a77bcfe704e3f7f6bef7c3d2
Minh Pham <pdminh0410@gmail.com>
2024-01-05 20:57:44 -0800

    Section 6.66: Using Tweens For Animations

--- animdo/lib/pages/home_page.dart
+++ animdo/lib/pages/home_page.dart
@@ -10,6 +10,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   double _buttonRadius = 100;
 
+  final Tween<double> _backgroundScale = Tween(
+    begin: 0.0,
+    end: 1.0,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,8 +31,19 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _pageBackground() {
-    return Container(
-      color: Colors.blue,
+    return TweenAnimationBuilder(
+      tween: _backgroundScale,
+      curve: Curves.easeInOutCubicEmphasized,
+      duration: Duration(seconds: 1),
+      builder: (_context, double _scale, _child) {
+        return Transform.scale(
+          scale: _scale,
+          child: _child,
+        );
+      },
+      child: Container(
+        color: Colors.blue,
+      ),
     );
   }
 
