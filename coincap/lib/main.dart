a3edc7a9b8d09bbada7d222a97795e3b72c817df
Minh Pham <pdminh0410@gmail.com>
2024-01-06 22:16:34 -0800

    Section 7.73: Loading and Reading Files In Flutter

--- coincap/lib/main.dart
+++ coincap/lib/main.dart
@@ -1,8 +1,24 @@
+import 'dart:convert';
+
+import 'package:coincap/models/app_config.dart';
 import 'package:coincap/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await loadConfig();
   runApp(const MyApp());
+}
+
+Future<void> loadConfig() async {
+  String _configContent =
+      await rootBundle.loadString("assets/config/main.json");
+  Map _configData = jsonDecode(_configContent);
+  GetIt.instance.registerSingleton<AppConfig>(
+    AppConfig(COIN_API_BASE_URL: _configData["COIN_API_BASE_URL"]),
+  );
 }
 
 class MyApp extends StatelessWidget {
