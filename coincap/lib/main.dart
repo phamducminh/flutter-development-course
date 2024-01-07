73ff4513704da545149188e33b010407dc13aa65
Minh Pham <pdminh0410@gmail.com>
2024-01-06 22:30:15 -0800

    Section 7.74: Creating HTTP Service & GetIt Package

--- coincap/lib/main.dart
+++ coincap/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:coincap/models/app_config.dart';
 import 'package:coincap/pages/home_page.dart';
+import 'package:coincap/services/http_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
@@ -9,6 +10,7 @@ import 'package:get_it/get_it.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await loadConfig();
+  registerHTTPService();
   runApp(const MyApp());
 }
 
@@ -18,6 +20,12 @@ Future<void> loadConfig() async {
   Map _configData = jsonDecode(_configContent);
   GetIt.instance.registerSingleton<AppConfig>(
     AppConfig(COIN_API_BASE_URL: _configData["COIN_API_BASE_URL"]),
+  );
+}
+
+void registerHTTPService() {
+  GetIt.instance.registerSingleton<HTTPService>(
+    HTTPService(),
   );
 }
 
