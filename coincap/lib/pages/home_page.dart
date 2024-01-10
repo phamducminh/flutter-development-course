21a68a4ca1a3b599a8400814ad88af670187960e
Minh Pham <pdminh0410@gmail.com>
2024-01-09 21:05:09 -0800

    Section 7.76: Implementing Coin Selection Dropdown

--- coincap/lib/pages/home_page.dart
+++ coincap/lib/pages/home_page.dart
@@ -1,7 +1,8 @@
+import 'package:coincap/services/http_service.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 class HomePage extends StatefulWidget {
-  
   @override
   State<StatefulWidget> createState() {
     return _HomePageState();
@@ -9,9 +10,70 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double? _deviceHeight, _deviceWidth;
+
+  HTTPService? _http;
+
+  @override
+  void initState() {
+    super.initState();
+    _http = GetIt.instance.get<HTTPService>();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _selectedCoinDropDown(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _selectedCoinDropDown() {
+    List<String> _coins = ["bitcoin"];
+    List<DropdownMenuItem<String>> _items = _coins
+        .map(
+          (e) => DropdownMenuItem(
+            value: e,
+            child: Text(
+              e,
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.w600),
+            ),
+          ),
+        )
+        .toList();
+
+    return DropdownButton(
+      value: _coins.first,
+      items: _items,
+      onChanged: (_value) {},
+      dropdownColor: const Color.fromRGBO(
+        83,
+        88,
+        206,
+        1.0,
+      ),
+      iconSize: 30,
+      icon: const Icon(
+        Icons.arrow_drop_down_sharp,
+        color: Colors.white,
+      ),
+      underline: Container(),
+    );
   }
 }
