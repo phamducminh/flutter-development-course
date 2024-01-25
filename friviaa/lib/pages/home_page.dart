6fdc8f51dc80984e8d2b545d539cd1d7561759e5
Minh Pham <pdminh0410@gmail.com>
2024-01-24 21:53:18 -0800

    Section 8.104: Navigating To Game Page

--- friviaa/lib/pages/home_page.dart
+++ friviaa/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:friviaa/pages/game_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -29,6 +30,7 @@ class _HomePageState extends State<HomePage> {
               children: [
                 _appTitle(),
                 _difficultySlider(),
+                _startGameButton(),
               ],
             ),
           ),
@@ -72,6 +74,31 @@ class _HomePageState extends State<HomePage> {
           _currentDifficultyLevel = _value;
         });
       },
+    );
+  }
+
+  Widget _startGameButton() {
+    return MaterialButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext _context) {
+              return GamePage();
+            },
+          ),
+        );
+      },
+      color: Colors.blue,
+      minWidth: _deviceWidth! * 0.80,
+      height: _deviceHeight! * 0.10,
+      child: const Text(
+        "Start",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 25,
+        ),
+      ),
     );
   }
 }
