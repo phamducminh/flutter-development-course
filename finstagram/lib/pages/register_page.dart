f7e403b0ec1f4253af6d2b48c4959f0cdaf6e05c
Minh Pham <pdminh0410@gmail.com>
2024-01-29 18:59:43 -0800

    Section 9.115: Register Page UI

--- finstagram/lib/pages/register_page.dart
+++ finstagram/lib/pages/register_page.dart
@@ -8,11 +8,58 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  double? _deviceHeight, _deviceWidth;
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: Container(
-        color: Colors.red,
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: _deviceWidth! * 0.05,
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _titleWidget(),
+                _registerButton(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _titleWidget() {
+    return const Text(
+      "Finstagram",
+      style: TextStyle(
+        fontSize: 25,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+
+  Widget _registerButton() {
+    return MaterialButton(
+      onPressed: () {},
+      minWidth: _deviceWidth! * 0.50,
+      height: _deviceHeight! * 0.05,
+      color: Colors.red,
+      child: const Text(
+        "Register",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w400,
+        ),
       ),
     );
   }
