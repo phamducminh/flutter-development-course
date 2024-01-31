97da4f19d89ff5394ec6bb3404103971a8a9b402
Minh Pham <pdminh0410@gmail.com>
2024-01-30 19:47:17 -0800

    Section 9.121: Bottom Navigation Bar Widget

--- finstagram/lib/pages/home_page.dart
+++ finstagram/lib/pages/home_page.dart
@@ -8,30 +8,69 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _currentPage = 0;
+  List<Widget> _pages = [
+    Container(
+      color: Colors.green,
+    ),
+    Container(
+      color: Colors.blue,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Finstagram",
-        ),
-        actions: [
-          GestureDetector(
-            onTap: () {},
-            child: const Icon(Icons.add_a_photo),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 8.0,
-              right: 8.0,
-            ),
-            child: GestureDetector(
-              onTap: () {},
-              child: const Icon(Icons.logout),
-            ),
-          )
-        ],
+      appBar: _appBar(),
+      bottomNavigationBar: _bottomNavigationBar(),
+      body: _pages[_currentPage],
+    );
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      title: const Text(
+        "Finstagram",
       ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: const Icon(Icons.add_a_photo),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 8.0,
+            right: 8.0,
+          ),
+          child: GestureDetector(
+            onTap: () {},
+            child: const Icon(Icons.logout),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _bottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: _currentPage,
+      onTap: (_index) {
+        setState(() {
+          _currentPage = _index;
+        });
+      },
+      items: const [
+        BottomNavigationBarItem(
+          label: "Feed",
+          icon: Icon(Icons.feed),
+        ),
+        BottomNavigationBarItem(
+          label: "Profile",
+          icon: Icon(
+            Icons.account_box,
+          ),
+        ),
+      ],
     );
   }
 }
