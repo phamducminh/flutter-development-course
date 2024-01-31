32856daf2944b344cfa09d26c4b4005a42e7715d
Minh Pham <pdminh0410@gmail.com>
2024-01-30 19:23:33 -0800

    Section 9.117: Selecting Files From Device Storage

--- finstagram/lib/pages/register_page.dart
+++ finstagram/lib/pages/register_page.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -15,6 +18,8 @@ class _RegisterPageState extends State<RegisterPage> {
   String? _name;
   String? _email;
   String? _password;
+
+  File? _image;
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +39,7 @@ class _RegisterPageState extends State<RegisterPage> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 _titleWidget(),
+                _profileImageWidget(),
                 _registrationForm(),
                 _registerButton(),
               ],
@@ -68,6 +74,31 @@ class _RegisterPageState extends State<RegisterPage> {
             _emailTextField(),
             _passwordTextField(),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _profileImageWidget() {
+    var _imageProvider = _image != null
+        ? FileImage(_image!)
+        : const NetworkImage("https://i.pravatar.cc/300");
+    return GestureDetector(
+      onTap: () {
+        FilePicker.platform.pickFiles(type: FileType.image).then((_result) {
+          setState(() {
+            _image = File(_result!.files.first.path!);
+          });
+        });
+      },
+      child: Container(
+        height: _deviceHeight! * 0.15,
+        width: _deviceHeight! * 0.15,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: _imageProvider as ImageProvider,
+          ),
         ),
       ),
     );
