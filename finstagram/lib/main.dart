0ee3fabfad3327e9775919744972cda7a2afad17
Minh Pham <pdminh0410@gmail.com>
2024-02-08 16:44:30 -0800

    Section 10.131: Creating Firebase Service Class

--- finstagram/lib/main.dart
+++ finstagram/lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:finstagram/pages/home_page.dart';
 import 'package:finstagram/pages/login_page.dart';
 import 'package:finstagram/pages/register_page.dart';
+import 'package:finstagram/services/firebase_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  GetIt.instance.registerSingleton<FirebaseService>(
+    FirebaseService(),
+  );
   runApp(const MyApp());
 }
 
