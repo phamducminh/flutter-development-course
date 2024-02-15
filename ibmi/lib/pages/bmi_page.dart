e65396d14a059339b3d843ee20e4ba16f5a8c9c2
Minh Pham <pdminh0410@gmail.com>
2024-02-14 16:03:54 -0800

    Section 11.146: Box Shadows In Flutter

--- ibmi/lib/pages/bmi_page.dart
+++ ibmi/lib/pages/bmi_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ibmi/widgets/info_card.dart';
 
 class BMIPage extends StatefulWidget {
   @override
@@ -9,11 +10,22 @@ class BMIPage extends StatefulWidget {
 }
 
 class _BMIPageState extends State<BMIPage> {
+  double? _deviceHeight, _deviceWidth;
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
     return CupertinoPageScaffold(
       child: Container(
-        color: Colors.red,
+        child: Center(
+          child: InfoCard(
+            height: _deviceHeight! * 0.20,
+            width: _deviceWidth! * 0.45,
+            child: Container(),
+          ),
+        ),
       ),
     );
   }
