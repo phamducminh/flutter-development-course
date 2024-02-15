34f044903b877bf7850d5c9ea8655745f5c33eb8
Minh Pham <pdminh0410@gmail.com>
2024-02-14 19:32:50 -0800

    Section 11.147: CupertinoDialogAction Widget

--- ibmi/lib/pages/bmi_page.dart
+++ ibmi/lib/pages/bmi_page.dart
@@ -12,6 +12,8 @@ class BMIPage extends StatefulWidget {
 class _BMIPageState extends State<BMIPage> {
   double? _deviceHeight, _deviceWidth;
 
+  int _age = 25;
+
   @override
   Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
@@ -20,12 +22,73 @@ class _BMIPageState extends State<BMIPage> {
     return CupertinoPageScaffold(
       child: Container(
         child: Center(
-          child: InfoCard(
-            height: _deviceHeight! * 0.20,
-            width: _deviceWidth! * 0.45,
-            child: Container(),
-          ),
+          child: _ageSelectContain(),
         ),
+      ),
+    );
+  }
+
+  Widget _ageSelectContain() {
+    return InfoCard(
+      height: _deviceHeight! * 0.20,
+      width: _deviceWidth! * 0.45,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Age yr',
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          Text(
+            _age.toString(),
+            style: TextStyle(
+              fontSize: 45,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 50,
+                child: CupertinoDialogAction(
+                  onPressed: () {
+                    setState(() {
+                      _age--;
+                    });
+                  },
+                  textStyle: const TextStyle(
+                    fontSize: 25,
+                    color: Colors.red,
+                  ),
+                  child: const Text('-'),
+                ),
+              ),
+              SizedBox(
+                width: 50,
+                child: CupertinoDialogAction(
+                  onPressed: () {
+                    setState(() {
+                      _age++;
+                    });
+                  },
+                  textStyle: const TextStyle(
+                    fontSize: 25,
+                    color: Colors.blue,
+                  ),
+                  child: const Text('+'),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
