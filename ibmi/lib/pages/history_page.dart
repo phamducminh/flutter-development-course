686a50558ff5afcf6f8526f5ea30f675ccf70de7
Minh Pham <pdminh0410@gmail.com>
2024-02-15 21:10:09 -0800

    Section 11.154: History Page UI

--- ibmi/lib/pages/history_page.dart
+++ ibmi/lib/pages/history_page.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ibmi/widgets/info_card.dart';
 
 class HistoryPage extends StatelessWidget {
+  double? _deviceHeight, _deviceWidth;
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
     return CupertinoPageScaffold(
-      child: Container(
-        color: Colors.green,
+      child: _dataCard(),
+    );
+  }
+
+  Widget _dataCard() {
+    return Center(
+      child: InfoCard(
+        height: _deviceHeight! * 0.25,
+        width: _deviceWidth! * 0.75,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _statusText("Normal"),
+            _dateText('02/15/2024'),
+            _bmiText('26.98'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _statusText(String _status) {
+    return Text(
+      _status,
+      style: const TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+
+  Widget _dateText(String _date) {
+    return Text(
+      _date,
+      style: const TextStyle(
+        fontSize: 15,
+        fontWeight: FontWeight.w300,
+      ),
+    );
+  }
+
+  Widget _bmiText(String _bmi) {
+    return Text(
+      _bmi,
+      style: const TextStyle(
+        fontSize: 60,
+        fontWeight: FontWeight.w600,
       ),
     );
   }
