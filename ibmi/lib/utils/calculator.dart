c3fae5b8fc0e841b6937760fc6c7a90245b4e465
Minh Pham <pdminh0410@gmail.com>
2024-02-16 21:25:00 -0800

    Section 12.159: Dependency Mocking using Mocktail

--- ibmi/lib/utils/calculator.dart
+++ ibmi/lib/utils/calculator.dart
@@ -1,5 +1,18 @@
+import 'dart:convert';
 import 'dart:math';
+
+import 'package:dio/dio.dart';
 
 double calculateBMI(int height, int weight) {
   return 703 * (weight / pow(height, 2));
+}
+
+Future<double> calculateBMIAsync(Dio _dio) async {
+  var _result = await _dio.get('https://jsonkeeper.com/b/AKFA');
+  var _data = jsonDecode(_result.data) as Map;
+  var _bmi = calculateBMI(
+    _data['Sergio Ramos'][0],
+    _data['Sergio Ramos'][1],
+  );
+  return _bmi;
 }
--- ibmi/test/utils/calculator_test.dart
+++ ibmi/test/utils/calculator_test.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ibmi/utils/calculator.dart';
+import 'package:mocktail/mocktail.dart';
+
+class DioMock extends Mock implements Dio {}
 
 void main() {
   test(
@@ -11,6 +17,31 @@ void main() {
       double bmi = calculateBMI(height, weight);
       // Assert
       expect(bmi, 22.955102040816328);
+    },
+  );
+
+  test(
+    'Given url When calculateBMIAsync invoked Then correct BMI returned',
+    () async {
+      // Arrange
+      final _dioMock = DioMock();
+      when(() => _dioMock.get('https://jsonkeeper.com/b/AKFA')).thenAnswer(
+        (_) => Future.value(
+          Response(
+            requestOptions:
+                RequestOptions(path: 'https://jsonkeeper.com/b/AKFA'),
+            data: jsonEncode(
+              {
+                "Sergio Ramos": [72, 165]
+              },
+            ),
+          ),
+        ),
+      );
+      // Act
+      var _result = await calculateBMIAsync(_dioMock);
+      // Assert
+      expect(_result, 22.375578703703706);
     },
   );
 }
